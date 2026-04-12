264b566387e07bb346fa1a267083d93b17858b3f
eedu7 <mueedahmad067@gmail.com>
2026-04-12 20:58:09 +0500

    feat(repositories): Introduce UserRepository for user data access
    Adds a dedicated repository pattern implementation for user-related database operations. This improves the separation of concerns by encapsulating all logic related to interacting with the `DBUser` model into `UserRepository`, making the codebase cleaner and easier to maintain.

---
+++ app/repositories/user.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import DBUser
+from core.repository import BaseRepository
+
+
+class UserRepository(BaseRepository[DBUser]):
+    def __init__(self, session: AsyncSession):
+        super().__init__(DBUser, session)
