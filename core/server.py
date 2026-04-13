429e3287fba3d0cfdef51c06c3847b1415bb6f59
eedu7 <mueedahmad067@gmail.com>
2026-04-13 10:54:41 +0500

    feat(server): Implement CORS middleware for FastAPI application
    Adds CORS middleware to the FastAPI application instance in core/server.py. This ensures that the backend API can securely handle cross-origin requests by using configurations defined in the application settings.

--- core/server.py
+++ core/server.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import router
+from core.config import config
+
+
+def make_middleware(app: FastAPI) -> None:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=config.CORS_ALLOW_ORIGINS,
+        allow_credentials=config.CORS_ALLOW_CREDENTIALS,
+        allow_methods=config.CORS_ALLOW_METHODS,
+        allow_headers=config.CORS_ALLOW_HEADERS,
+    )
 
 
 def init_routers(app_: FastAPI) -> None:
@@ -10,4 +22,5 @@ def init_routers(app_: FastAPI) -> None:
 def run_server() -> FastAPI:
     app_ = FastAPI(title="ECommerce Backend")
     init_routers(app_)
+    make_middleware(app_)
     return app_
